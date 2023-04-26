3b69cb1d5594921b1e8a438658e4d9663d698a17
Jonathan Randen <jonathanranden@gmail.com>
2023-04-26 15:03:56 +0200

    finished game

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import {
-    getFirestore, collection, getDocs, addDoc
+    getFirestore, collection, onSnapshot, addDoc, doc, query, where, orderBy
 } from 'firebase/firestore'
 
 const firebaseConfig = {
@@ -22,52 +22,120 @@ const firebaseConfig = {
   // collection ref
   const colRef = collection(db,'Highscores')
 
-  // Get collection data
-  getDocs(colRef)
-    .then((snapshot) => {
-        let Highscores = []
-        snapshot.docs.forEach((doc) => {
-            Highscores.push({...doc.data(), id: doc.id})
-        })
-        console.log(Highscores)
-    })
-    .catch(err => {
-        console.log(err.message)
-    })
 
-    // Addoing documents 
-    const addUser = document.querySelector('.user')
-    addUser.addEventListener ('submit', (e) => {
-        e.preventDefault()
-        list.textContent = `Level`;
+  // queries
+  const q = query(colRef, orderBy('Score', 'asc'))
 
-        addDoc(colRef, {
-            Person: addUser.name.value
-        })
-        .then(() => {
-            addUser.reset()
-        })
+  // Get real time collection data
+onSnapshot(q,(snapshot) => {
+    let highscores =[]
+    snapshot.docs.forEach((doc) => {
+        highscores.push({...doc.data(), id: doc.id})
+    })
+   
+    removeList()
+    createList(highscores)
+    console.log(highscores)
+})
+
+//add documents tar verdien fra navn inputen og sender det inn også fjerner navne feltet og 
+const addUser = document.querySelector('.user')
+addUser.addEventListener ('submit', (e) => {
+    e.preventDefault()
+
+    addDoc(colRef, {
+        Person: addUser.name.value
     })
 
-    //-------------------------------------------------------//
-    // GAME FUNCTIONS//
+    gameField.classList.remove("hidden")
+    nameField.classList.add("hidden")
+})
+
+
+
     let correctAnswer = Math.floor (Math.random() *100);
-console.log(correctAnswer)
+    console.log(correctAnswer)
+    
+    let count = 0
+    let score;
+    
+    const input = document.querySelector('#number');
+    const tall = document.querySelector('#feedback');
+    const gameField = document.querySelector('.game')
+    const nameField = document.querySelector('.user')
+    const scoreboard = document.querySelector('#highscorelist')
+    const scoreboardlength = 10
+    
+    
 
-const input = document.querySelector('#number');
-const tall = document.querySelector('#feedback');
 
-input.addEventListener ('keydown', e=> {
+input.addEventListener ('keyup', e=> {
+    if (e.key == "Enter") {
     const number = input.value * 1
     if(number === correctAnswer){
     tall.textContent = 'This is the correct number, Well done!'
+    stopTimer()
+    addScore()
+    addDoc(colRef, {
+        Person: addUser.name.value,
+        Score: count
+    })
+
     }
      else if(number > correctAnswer) {
     tall.textContent ='The number you are looking for is lower'
+    startTimer()
+    addScore()
     }
     else if(number < correctAnswer){
     tall.textContent = 'The number you are looking for is higher'
-    }
+    startTimer()
+    addScore()
+}
+
+
+}
 });
 
+function counter() {
+    count++;
+    
+    console.log(count)
+    }
+    
+    function addScore(){
+        count +=10
+    }
+    
+    function startTimer() {
+        score = setInterval(counter,100);
+        
+    }
+    
+    function stopTimer(){
+        clearInterval(score)
+    }
+counter()
+
+
+function  createList (par1) {
+    for (let i = 0; i < par1.length && i < scoreboardlength; i++) {
+
+        const li = document.createElement('li')
+        li.textContent =`${i+1}. Name : ${par1[i].Person} Score : ${par1[i].Score}`;
+        li.classList.add("scorelistli")
+
+        scoreboard.append(li)
+
+        
+    }
+}
+
+function removeList (){
+    const removeElement = document.querySelectorAll('.scorelistli');
+
+    removeElement.forEach(element => {
+        element.remove();
+    })
+}
 
